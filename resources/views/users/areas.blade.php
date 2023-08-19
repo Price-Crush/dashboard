c70e8f484b4a753b3d0954d6423a8fbfaae87a01
tiganii <tigani.ismail@gmail.com>
2023-08-19 12:56:53 +0200

    adding areas to user

--- resources/views/users/areas.blade.php
+++ resources/views/users/areas.blade.php
@@ -14,7 +14,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title"> Areas Management</h4>
+                    <h4 class="card-title"> My Areas </h4>
                 </div>
 
                 <div class="card-content">
